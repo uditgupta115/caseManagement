a54d717f153b1ac469abeffbbd0d4416e9352293
Udit <uditgupta115@gmail.com>
2020-02-03 17:46:46 +0530

    mysql remote setup

--- settings/development.py
+++ settings/development.py
@@ -86,22 +86,23 @@ WSGI_APPLICATION = 'casemanagement.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-        # 'default': {
-        #     'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        #     # 'NAME': 'ka01BFfxz2',
-        #     # 'USER': 'ka01BFfxz2',
-        #     # 'PASSWORD': '01wpjKuCj4',
-        #     # 'HOST': '37.59.55.185',
-        #     'NAME': 'casesystem',
-        #     'USER': 'root',
-        #     'PASSWORD': 'root',
-        #     'HOST': 'localhost',
-        #     'PORT': '3306',
-        # },
         'default': {
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            'ENGINE': 'django.db.backends.sqlite3',
+            'ENGINE': 'django.contrib.gis.db.backends.mysql',
+            'NAME': 'rN84yz7dkr',
+            'USER': 'rN84yz7dkr',
+            'PASSWORD': 'HVq09AkE48',
+            # 'HOST': '37.59.55.185',
+            'HOST': 'remotemysql.com',
+            # 'NAME': 'casesystem',
+            # 'USER': 'root',
+            # 'PASSWORD': 'root',
+            # 'HOST': 'localhost',
+            'PORT': '3306',
         },
+        # 'default': {
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        # },
 }
 
 AUTH_USER_MODEL = 'casesystem.User'
